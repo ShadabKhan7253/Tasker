101b1a68532d02c96ea430aca47082a40a0d4ebe
Shadab Khan <shadabkhan7253@gmail.com>
2023-11-16 20:33:51 +0530

    Handle input field

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container } from '@mui/material';
 import Header from './components/Header';
 import TaskForm from './TaskForm';
 import TaskList from './TaskList';
 
 const App = () => {
+  const [taskInput, setTaskInput] = useState('');
+
+  const handleTaskInputChange = (evt) => {
+    setTaskInput(evt.target.value);
+  };
+
+  const handleTaskInputClear = () => {
+    setTaskInput('');
+  };
+
   return (
     <div>
       <Header />
       <Container maxWidth="lg" sx={{ marginTop: 4 }}>
-        <TaskForm />
+        <TaskForm
+          taskInput={taskInput}
+          onTaskInputChnage={handleTaskInputChange}
+          onClear={handleTaskInputClear}
+        />
         <TaskList />
       </Container>
     </div>
--- src/TaskForm.js
+++ src/TaskForm.js
@@ -3,14 +3,20 @@ import { Card, TextField, Button, Grid } from '@mui/material';
 import DataSaverOnOutlinedIcon from '@mui/icons-material/DataSaverOnOutlined';
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 
-const TaskForm = () => {
+const TaskForm = ({ taskInput, onTaskInputChnage, onClear }) => {
   return (
     <Grid container justifyContent="center">
       <Grid item xs={12}>
         <Card sx={{ padding: 4 }} raised="true">
           <Grid container spacing={2} alignItems="center">
             <Grid item xs={8}>
-              <TextField id="task-input" label="Insert your task" fullWidth />
+              <TextField
+                id="task-input"
+                value={taskInput}
+                label="Insert your task"
+                fullWidth
+                onChange={onTaskInputChnage}
+              />
             </Grid>
             <Grid item xs={2}>
               <Button variant="contained" color="primary" fullWidth>
@@ -18,7 +24,7 @@ const TaskForm = () => {
               </Button>
             </Grid>
             <Grid item xs={2}>
-              <Button variant="contained" color="secondary" fullWidth>
+              <Button variant="contained" color="secondary" fullWidth onClick={onClear}>
                 <DeleteOutlinedIcon /> Clear
               </Button>
             </Grid>
